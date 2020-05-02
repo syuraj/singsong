9590bff67f306e0b22eeb134f63a1ca5653d9189
Suraj Shrestha <syuraj@gmail.com>
2020-05-02 18:41:49 -0400

    adding github action

---
+++ rename_package.sh
@@ -0,0 +1,20 @@
+
+
+# PACKAGE_NAME="com.vynilla.beta"
+# PROJECT_NAME=
+# BETA_APP_NAME=""
+TIME_STAMP=$(date +%s)
+BUILD_GRADLE=./android/app/build.gradle
+# ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
+# ANDROID_MANIFEST_FILE=./android/app/src/main/AndroidManifest.xml
+# GOOGLE_SERVICES_FILE=./android/app/google-services.json
+# INFO_PLIST_FILE=ios/$PROJECT_NAME/Info.plist
+
+# sed -i '' 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+sed -i '' 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
+
+# sed -i '' 's/"app_name">[^<]*</"app_name">'$BETA_APP_NAME'</' $ANDROID_STRINGS_FILE
+# sed -i '' 's/"@mipmap\/ic_launcher"/"@mipmap\/ic_launcher_beta"/' $ANDROID_MANIFEST_FILE
+# sed -i '' 's/"package_name": "[^"]*"/"package_name": "'$PACKAGE_NAME'"/' $GOOGLE_SERVICES_FILE
+
+# plutil -replace CFBundleIdentifier -string $PACKAGE_NAME $INFO_PLIST_FILE
